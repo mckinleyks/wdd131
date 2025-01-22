e4b8fa9e114ef0dcded3d5f22572358d328e127c
mckinleyks <sau24010@byui.com>
2025-01-21 17:07:01 -0700

    large screen updated

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,30 +1,45 @@
-const hamburgerButton = document.getElementById("hamburger");
-const menu = document.getElementById("menu");
-const closeButton = document.getElementById("close");
-
-hamburgerButton.addEventListener("click", () => {
-    if (menu.classList.contains("hidden")) {
-        menu.classList.remove("hidden");
-        menu.classList.add("visible");
-        hamburgerButton.innerHTML = "&#10006;";
-    } else {
-        menu.classList.remove("visible");
-        menu.classList.add("hidden");
-        hamburgerButton.innerHTML = "&#9776";
-    }
-});
-
-closeButton.addEventListener("click", () => {
-    menu.classList.remove("visible");
-    menu.classList.add("hidden");
-    hamburgerButton.innerHTML = "&#9776;";
-})
-
-const yearElement = document.getElementById("year");
-const currentYear = new Date().getFullYear();
-yearElement.textContent = currentYear;
-
 document.addEventListener("DOMContentLoaded", function() {
+    const hamburgerButton = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+    const closeButton = document.getElementById("close");
+
+
+
+    if (hamburgerButton && menu && closeButton) {
+        hamburgerButton.addEventListener("click", () => {
+            if (menu.classList.contains("hidden")) {
+                menu.classList.remove("hidden");
+                menu.classList.add("visible");
+                hamburgerButton.style.display = "none";
+                closeButton.style.display = "block";
+
+            } else {
+                menu.classList.remove("visible");
+                menu.classList.add("hidden");
+                hamburgerButton.style.display = "block";
+                closeButton.style.display = "none";
+            }
+        });
+
+        closeButton.addEventListener("click", () => {
+            menu.classList.remove("visible");
+            menu.classList.add("hidden");
+            hamburgerButton.style.display = "block";
+            closeButton.style.display = "none";
+        });
+
+    } else {
+        console.error("Elements not found: hamburgerButton, menu, or closeButton");
+    }
+
+    const yearElement = document.getElementById("year");
+    const currentYear = new Date().getFullYear();
+    if (yearElement) {
+        yearElement.textContent = currentYear;
+    } else {
+        console.log("Element with ID 'year' not found.");
+    }
+
     const lastModifiedElement = document.getElementById("last-modified");
     if (lastModifiedElement) {
         const lastModifiedDate = document.lastModified;
@@ -32,7 +47,6 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
         console.log("Element with ID 'last-modified' not found.");
     }
-
 });
 
 
